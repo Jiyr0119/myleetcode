f96af5a05fc573aad9f7a8e1ddeda3ede1825057
Jonathan <594402100@qq.com>
2019-10-15 20:16:32 +0800

    all - 7

--- 6.z-字形变换.js
+++ 6.z-字形变换.js
@@ -9,6 +9,21 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
-    
+    var len=s.length;
+   var twoRows=2*numRows-2;
+   var str="";
+   if(len==0||numRows<=1)
+   return s;
+   for(i=0;i<numRows;i++){
+       for(j=i;j<len;j+=twoRows){
+           str=str.concat(s.charAt(j));
+
+           if(i!=0&&i!=numRows-1&&j-2*i+twoRows<len){
+               str=str.concat(s.charAt(j-2*i+twoRows))     
+        }
+       }
+   }
+   return str
+
 };
 
